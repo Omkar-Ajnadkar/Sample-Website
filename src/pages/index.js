8190a0df207166b243c172b4a0dd79f79a0f24bc
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2018-08-30 20:23:08 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your my new Gatsby site.</p>
+    <p>This website is built by Omkar</p>
+    <p>This is just the start. Something big is coming...</p>
+    <Link to="/page-2/">Page 2</Link>
+  </div>
+)
+
+export default IndexPage
